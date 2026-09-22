06ab5f2cd120679975fb77e9b9992993603fa490
cmachsocket <cmach_socket@outlook.com>
2026-09-23 01:37:31 +0800

    fix double hook for litellm

--- plugins/chat_bot/chat_bot.py
+++ plugins/chat_bot/chat_bot.py
@@ -3,8 +3,6 @@ from ncatbot.event.qq import GroupMessageEvent
 from ncatbot.plugin import NcatBotPlugin
 import hindsight_litellm
 from hindsight_client import Hindsight
-from hindsight_litellm.callbacks import HindsightCallback
-import litellm
 from datetime import datetime
 import json
 from typing import Any, Dict, List
@@ -61,10 +59,8 @@ class AIHelloWorldPlugin(NcatBotPlugin):
         self.target_group_id = self.get_config("TARGET_GROUP_ID", 1093424135)
         self.hindsight = Hindsight(base_url=f"http://localhost:{self.hindsight_port}") 
         hindsight_litellm.configure(hindsight_api_url=f"http://localhost:{self.hindsight_port}")
-        hindsight_litellm.set_defaults(bank_id="default-bank")  # 设置一个默认值
+        #hindsight_litellm.set_defaults(bank_id="default-bank")  # 设置一个默认值
         hindsight_litellm.enable()
-        hindsight_callback = HindsightCallback()
-        litellm.callbacks = [hindsight_callback]
         info = await self.api.qq.query.get_login_info()
         self.bot_id = info.user_id
     @registrar.qq.on_group_message()
